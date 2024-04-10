79816638cf6c8ffb3e20335956f749bd0988860a
Martha Irmentrud <martha.irmentrud@tutanota.com>
2024-04-11 01:29:54 +0200

    test: check build not passing

--- test_main.py
+++ test_main.py
@@ -38,3 +38,7 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+
+def test_build_not_passing():
+    assert False
